1090b5e9aaf109bc32592279ba13c1532f3b6612
Geovanny Lacayo <geovannylacayo@gmail.com>
2021-06-27 18:24:38 -0600

    Create data base, model and test data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        //Dont use User Factory, because that file create random users
+        App\User::create([
+            'name' => 'Geovanny Lacayo',
+            'email' => 'g@admin.com',
+            'password' => bcrypt('123456')
+        ]);
+
+        factory(App\Post::class, 24)->create();
     }
 }
